56f8f4c34f6d9dd9bff5d62547568912c5056b5d
Rickey Hrabowskie <rickeyhrabowskie@Rickeys-MacBook-Pro.local>
2016-08-29 15:39:45 -0500

    first commit

---
+++ Pods/Target Support Files/Pods-PokeFinder/Pods-PokeFinder-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_PokeFinderVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_PokeFinderVersionString[];
+
---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Rickey Hrabowskie on 8/29/16.
+//  Copyright © 2016 Rickey Hrabowskie. All rights reserved.
+//
+
+#import "GeoFire.h" 
